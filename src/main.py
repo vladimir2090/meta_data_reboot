1b7f404f66ff5d0506251af054c99fbab1b1bc50
Vladimir2090 <zvladzubkov@gmail.com>
2025-08-09 10:06:26 +0300

    add get_music_meta_data and expect for function

--- src/main.py
+++ src/main.py
@@ -1,5 +1,44 @@
+from mutagen.easyid3 import EasyID3
+from mutagen.mp3 import MP3
+#import re
+import os
+
+
+def get_music_meta_data(file_path):
+    music_tags = ['title', 'artist', 'album', 'date', 'genre', 'tracknumber', 'albumartist']
+    
+    try:
+        audio = EasyID3(file_path)
+        mp3_info = MP3(file_path)
+
+        if not os.path.exists(file_path):
+            print(f'File not found {file_path}')
+            return {}
+        
+        meta_data = {
+            tag: audio.get(tag, ['N (no data)'])[0]
+            for tag in music_tags
+            if tag in audio
+        }
+        
+        return meta_data
+        
+    except Exception as error:
+        print(f'Error: {error}')
+        return {}
+
+
 def main():
-    print("meta_data_reboot запущен!")
+    try:
+        file_path = r"C:\Users\admin\Desktop\music_noformat\2mashi_-_bosaya_(zaycev.net)-1.mp3"
+        
+        metadata = get_music_meta_data(file_path)
+        
+        for key, value in metadata.items():
+            print(f"{key}: {value}")
+
+    except Exception as error:
+        print(f'Error: {error}')
 
 if __name__ == "__main__":
     main()
