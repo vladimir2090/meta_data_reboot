df43662672cb38748b992fdd057940789352ee63
Vladimir2090 <zvladzubkov@gmail.com>
2025-08-17 19:36:31 +0300

    add setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,15 @@
-#code for setup
+from setuptools import setup, find_packages
+
+setup(
+    name="meta_data_reboot",
+    version="0.9.1",
+    description="A Python script for modifying file name and metadata using local AI assistance",
+    author="Vladimir2090",
+    url="https://github.com/vladimir2090/meta_data_reboot",
+    packages=find_packages(),
+    install_requires=[
+        "mutagen",
+        "llama_cpp"
+    ],
+    python_requires=">=3.9",
+)
